75848f09225c0e345a08e1912c57cb8b6efa20c1
Roushelfy <2100012985@stu.pku.edu.cn>
2024-07-29 00:29:38 -0400

    modify for mdbook

--- simulators/1_mass_spring/src/main.cpp
+++ simulators/1_mass_spring/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	double rho = 1000, k = 1e5, initial_stretch = 1.4, n_seg = 10, h = 0.004, side_len = 1, tol = 0.01;
+	double rho = 1000, k = 1e5, initial_stretch = 1.3, n_seg = 15, h = 0.004, side_len = 1, tol = 0.01;
 	MassSpringSimulator<double, 2> simulator(rho, side_len, initial_stretch, k, h, tol, n_seg);
 	simulator.run();
 }
